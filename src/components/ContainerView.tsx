47811fce79beb70d5218367ef644f77289283c0d
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2021-09-28 20:21:37 +0100

    fix: remove shadow from container view component

--- src/components/ContainerView.tsx
+++ src/components/ContainerView.tsx
@@ -1,6 +1,5 @@
 import React, {ReactNode} from 'react';
 import {StyleSheet, View, ViewStyle} from 'react-native';
-import {Styles} from '../theme/Styles';
 
 export function ContainerView(props: {
   children?: ReactNode;
@@ -11,7 +10,6 @@ export function ContainerView(props: {
 
 const styles = StyleSheet.create({
   container: {
-    ...Styles.Shadow(),
     paddingVertical: 16,
     flexDirection: 'row',
     justifyContent: 'space-between',
